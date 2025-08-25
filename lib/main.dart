9e487dce39d143e7bff16d628f3c1b62a457fcff
Analuizamunizdossantos <analuizzamunizz18@gmail.com>
2025-08-25 11:32:19 -0300

    criação de telas tipo home e login, sem o banco de dados.

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,20 @@
 import 'package:flutter/material.dart';
+import 'tela_login.dart';
 import 'usuario.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
 
-    usuario u = new usuario(
-    codigo: 1,
-    login: "Ana Luiza18",
-    nome: "Ana Luiza",
-    senha:"12346"
-    );
 
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Meus Dados"),
-        ),
-        body: Center(
-          child: Padding(padding: const EdgeInsets.all(18.0),
-            child: Column(
-              children: [
-                Text("Nome: ${u.nome}"),
-                Text("Código: ${u.codigo}"),
-                Text("Login: ${u.login}"),
-                Text("Senha: ${u.senha}"),
-              ],
-            ),
-          ),
-        ),
-      ),
+    debugShowCheckedModeBanner: false,
+      home: TelaLogin(),
     );
   }
 }
